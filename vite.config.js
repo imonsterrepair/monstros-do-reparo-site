7da8e773e3a3ec26edcd0c783e6789a8f99a3523
imonsterrepair <imonsterrepair.rj@gmail.com>
2025-06-21 15:58:00 -0300

    Fix: Confirmado base path no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  base: '/', // ESTA LINHA É CRUCIAL
+  base: '/', // Esta linha é crucial
 })
 
