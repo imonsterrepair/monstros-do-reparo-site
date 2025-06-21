39c6519c413132b5989235b4b7bde3c624d33884
imonsterrepair <imonsterrepair.rj@gmail.com>
2025-06-21 18:22:56 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SalesPage from './components/SalesPage';
+import CapturePage from './components/CapturePage';
+import ThankYouPage from './components/ThankYouPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SalesPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<SalesPage />} />
+          <Route path="/captura" element={<CapturePage />} />
+          <Route path="/obrigado" element={<ThankYouPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
-
-export default App;
