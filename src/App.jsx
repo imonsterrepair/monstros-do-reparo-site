6ea2bbda96f9bf8d911ed4084c7f0fa074245634
imonsterrepair <imonsterrepair.rj@gmail.com>
2025-06-21 16:44:04 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Assumindo que App.css estará na mesma pasta src
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Monstros do Reparo</h1>
+      <p>Página de Vendas, Captura e Obrigado</p>
+      {/* Seu conteúdo das páginas será renderizado aqui */}
+    </div>
+  );
+}
+
+export default App;
