8abcd3e040d2688056d90bd27022c5446dd84d98
imonsterrepair <imonsterrepair.rj@gmail.com>
2025-06-21 16:45:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  base: '/', // Esta linha é crucial
+  base: '/', // Esta linha é crucial para o deploy na raiz do domínio
 })
 
